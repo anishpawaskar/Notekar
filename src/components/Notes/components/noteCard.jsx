10bf8b59e73bf7d755bba80d5dca44814a9ed962
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-09 00:24:05 +0530

    Fix card redirecting issue

--- src/components/Notes/components/noteCard.jsx
+++ src/components/Notes/components/noteCard.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 export const NoteCard = ({ note }) => {
   return (
-    <Link to={`/${note.id}`}>
-      <div className="m-0 max-w-[250px] flex flex-col gap-4 mt-4 mx-auto rounded-md p-4 shadow-md border">
+    <Link className="block max-w-[250px] mt-4 mx-auto" to={`/${note.id}`}>
+      <div className="max-w-[250px] flex flex-col gap-4 mt-4 mx-auto rounded-md p-4 shadow-md border">
         {note.title && <p className="font-medium">{note.title}</p>}
         {note.description && <p className="text-lg">{note.description}</p>}
       </div>
