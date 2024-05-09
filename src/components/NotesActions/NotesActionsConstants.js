9da09d779f1d61c76ae4c959c8254acbcbbf62d1
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-10 04:50:07 +0530

    create notes actions component

---
+++ src/components/NotesActions/NotesActionsConstants.js
@@ -0,0 +1,11 @@
+import deleteIcon from './assets/delete-icon.png';
+
+export const NOTES_ACTIONS = [
+  {
+    id: '1',
+    name: 'Delete',
+    type: 'button',
+    imgUrl: deleteIcon,
+    actionType: 'delete',
+  },
+];
