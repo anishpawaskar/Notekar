9bac7ac188ccc0749c3ccb7abc5fcc0b17bfd3d6
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-13 02:11:16 +0530

    refactor css and add fallback label if the note and title are empty

--- src/components/Notes/components/noteCard.jsx
+++ src/components/Notes/components/noteCard.jsx
@@ -8,14 +8,21 @@ export const NoteCard = ({ note }) => {
         style={{
           background: note.theme?.backgroundColor,
         }}
-        className="max-w-[250px] flex flex-col gap-4 mt-4 mx-auto rounded-md p-4 shadow-md border"
+        className="max-w-[250px] max-h-[300px]  flex flex-col gap-4 mt-4 mx-auto rounded-md p-4 shadow-md border"
       >
         {note.title && (
-          <p className="font-medium whitespace-pre-wrap break-words">
+          <p className="font-medium whitespace-pre-wrap break-words max-h-[140px] overflow-hidden">
             {note.title}
           </p>
         )}
-        {note.description && <p className="text-lg">{note.description}</p>}
+        {note.description && (
+          <p className="text-lg whitespace-pre-wrap break-words max-h-[140px] overflow-hidden ">
+            {note.description}
+          </p>
+        )}
+        {note.title === '' && note.description === '' && (
+          <p className="m-0 text-xl">Empty note</p>
+        )}
       </div>
     </Link>
   );
