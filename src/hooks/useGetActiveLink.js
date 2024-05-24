28d420e547c05d9ba9dcc05417c9e553773f2c11
Anish Pawaskar <anishpawaskar@gmail.com>
2024-05-24 19:04:15 +0530

    create custom hook to get current active link

---
+++ src/hooks/useGetActiveLink.js
@@ -0,0 +1,17 @@
+import { useLocation } from 'react-router-dom';
+
+export const useGetActiveLink = () => {
+  const location = useLocation();
+  let activeLink;
+
+  if (location.pathname === '/' && location.search === '') {
+    activeLink = 'Notes';
+  } else if (location.search === '?archive=true') {
+    activeLink = 'Archive';
+  } else {
+    const label = location.search.split('=')[1].split('%20').join(' ');
+    activeLink = label;
+  }
+
+  return [activeLink];
+};
