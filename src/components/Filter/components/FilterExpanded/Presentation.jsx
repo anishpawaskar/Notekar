64eb4c486d0be231d5078783f09184213a28b0f4
Anish Pawaskar <anishpawaskar@gmail.com>
2024-06-03 02:55:28 +0530

    update routes to /notes

--- src/components/Filter/components/FilterExpanded/Presentation.jsx
+++ src/components/Filter/components/FilterExpanded/Presentation.jsx
@@ -23,7 +23,7 @@ export const FilterExpandedPresentation = ({
         <ul>
           <Link
             className={`${activeLink === 'Notes' && 'bg-[#e0e0e0]'} block`}
-            to="/"
+            to="/notes"
           >
             <li className="w-full hover:bg-[#e0e0e0]">
               <button
